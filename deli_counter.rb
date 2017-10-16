97ae8302b6f0c21657f949813614c79806be213f
Adam Clayman <adam@clayman.org>
2017-10-16 04:38:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(line)
     puts "The line is currently empty."
   else
     sentence = "The line is currently:"
-    line.each_with_index do |name|
+    line.each_with_index do |name, index|
       sentence += " #{index + 1}. #{name}"
     end
     binding.pry
