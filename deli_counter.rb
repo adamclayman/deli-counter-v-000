43db080f5a221f6431af32459de6e34fcb4dde68
Adam Clayman <adam@clayman.org>
2017-10-16 04:47:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,7 @@ end
 def now_serving(line)
   if line != []
     puts "Currently serving #{line[0]}"
-    katz_deli.shift
+    line.shift
   else
     puts "There is nobody waiting to be served!"
   end
