e572beeed3754d98ab01d4eed425349a9c551550
Adam Clayman <adam@clayman.org>
2017-10-16 04:31:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+katz_deli = []
+
+def line
+  if katz_deli == []
+    puts "The line is currently empty."
+  else
+    sentence = "The line is currently:"
+    katz_deli.each_with_index do |name|
+      sentence + " #{index + 1}. #{name}"
+    end
+    puts sentence
+  end
+end
